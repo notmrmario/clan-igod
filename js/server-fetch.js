5e16c653a8957e6463cba4794021ff49c16d24d6
Mario_iGod <nachohdgamingpro@gmail.com>
2020-12-14 21:32:58 +0100

    v5

--- js/server-fetch.js
+++ js/server-fetch.js
@@ -1,9 +1,10 @@
 fetch("https://eu.mc-api.net/v3/server/ping/notmrmario.aternos.me")
 .then(function (response)  {
-    return response.json();
+    return response.json()
 })
 .then(function (json) {
     document.querySelector('#stus').innerHTML = json.online ? 'Online' : 'Offline';
         document.querySelector("#nply").innerHTML = json.online ? 'Jugadores: ' + json.players.online + '/100' :'';
-        document.querySelector('#online')
+        document.querySelector('#motd').innerHTML = json.description ? json.description.text.replace(/\§4/gi, ' ').replace(/\§a/gi, ' ').replace(/\§d/gi, ' ').replace(/\§l/gi, ' ').replace(/\§2/gi, ' ').replace(/\§f/gi, ' ') : '';
+        document.querySelector('#server-icon').innerHTML = json.favicon ? json.favicon.url : '';
 })
