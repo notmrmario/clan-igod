a06044cb969924f6978deefbfc25cdbeee2cb194
Mario_iGod <nachohdgamingpro@gmail.com>
2020-12-15 18:09:17 +0100

    v5 y fixes

--- js/js.js
+++ js/js.js
@@ -5,6 +5,6 @@ fetch("https://eu.mc-api.net/v3/server/ping/notmrmario.aternos.me")
 .then(function (json) {
     document.querySelector('#stus').innerHTML = json.online ? 'Online' : 'Offline';
         document.querySelector("#nply").innerHTML = json.online ? 'Jugadores: ' + json.players.online + '/100' :'';
-        document.querySelector('#motd').innerHTML = json.description ? json.description.text.replace(/\§4/gi, ' ').replace(/\§a/gi, ' ').replace(/\§d/gi, ' ').replace(/\§l/gi, ' ').replace(/\§2/gi, ' ').replace(/\§f/gi, ' ') : '';
+        document.querySelector('#motd').innerHTML = json.description ? json.description.text.replace(/\§4/gi, ' ').replace(/\§a/gi, ' ').replace(/\§d/gi, ' ').replace(/\§l/gi, ' ').replace(/\§2/gi, ' ').replace(/\§f/gi, ' ').replace(/\§6/, ' ') : '';
         document.querySelector('#server-icon').innerHTML = json.favicon ? json.favicon.url : '';
 })
